803b65b6de081c1df60da73f32e249c85e31c083
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2016-01-27 09:49:12 +0000

    Excluded inland water bodies from sites for intersection.

--- apps/natura/scripts/intersection.py
+++ apps/natura/scripts/intersection.py
@@ -1,22 +1,39 @@
 from corine.models import Patch
+from django.db.models import Q
 from natura.models import Cover, IntersectionLog, Site
 from natura.tasks import process_sites
 
 
 def run():
-    nr_of_sites = Site.objects.count()
-    chunk_size = 50
-
     # Drop current cover objects
     Cover.objects.all().delete()
     IntersectionLog.objects.all().delete()
 
-    print('Processing {} Sites and {} Patches'.format(nr_of_sites, Patch.objects.count()))
+    # Select Natura sites that are not water bodies
+    non_water_sites = Site.objects.exclude(
+        Q(habitatclass__habitatcode='N06') & Q(habitatclass__percentagecover__gte=95)
+    )
+    water_sites = Site.objects.filter(
+        Q(habitatclass__habitatcode='N06') & Q(habitatclass__percentagecover__gte=95)
+    )
 
+    print('Processing {} Sites and {} Patches'.format(Site.objects.count(), Patch.objects.count()))
+
+    # Log how many sites are excluded
+    ws_count = water_sites.count()
+    if(ws_count > 0):
+        print('Excluded {} sites where inland water bodies are over 95% of its cover.'.format(ws_count))
+
+    # Set task batch size
+    batch_size = 50
+
+    # Set empty batch variable
     batch = []
-    for site in Site.objects.values('id').all():
+
+    # Create one celery task for each batch
+    for site in non_water_sites.values('id'):
         batch.append(site['id'])
-        if(len(batch) == chunk_size):
+        if(len(batch) == batch_size):
             process_sites.delay(batch)
             batch = []
 
