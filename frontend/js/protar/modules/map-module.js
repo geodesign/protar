d630a7e1a4581e53ef610ac105f4b5b6840c4f83
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2016-02-24 23:13:09 +0000

    Small changes on frontend map module.

--- frontend/js/protar/modules/map-module.js
+++ frontend/js/protar/modules/map-module.js
@@ -38,8 +38,23 @@ define([
                 });
 
                 // Base layer
-                L.tileLayer('http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png').addTo(LMap);
+                //L.tileLayer('http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png').addTo(LMap);
+
+                // Base layer
+                var basemap = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png',{
+                  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+                }).addTo(LMap);
+
                 L.tileLayer('/raster/tiles/2/{z}/{x}/{y}.png').addTo(LMap);
+
+                // Labelmap with streets
+                var labelmap = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png',{
+                  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+                }).addTo(LMap);
+
+                // Make sure streets are overlaid on top
+                LMap.getPanes().overlayPane.appendChild(labelmap.getContainer());
+                labelmap.setZIndex(9999);
             }
         });
 
