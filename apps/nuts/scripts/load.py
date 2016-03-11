eecae3078410b64b9c5ff2a5e743e97a0032d09b
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2016-03-11 15:18:53 +0000

    tried dissolving nuts boundaries.

--- apps/nuts/scripts/load.py
+++ apps/nuts/scripts/load.py
@@ -5,7 +5,8 @@ from django.contrib.gis.utils import LayerMapping
 from django.db.models import F
 from nuts.models import Nuts
 from nuts.scripts import const
-
+from django.contrib.gis.db.models import Collect, Union
+from django.contrib.gis.geos import MultiPolygon
 
 def run():
     # Get data directory from environment
@@ -23,7 +24,34 @@ def run():
 
     # Use layermapping to load all shapes
     lm = LayerMapping(Nuts, geosource, const.NUTS_FIELD_MAPPING)
-    lm.save(step=1000, progress=True)
+    lm.save(step=1000, progress=True, strict=True)
 
     # Copy country code into countryfield table
     Nuts.objects.all().update(country=F('n0cd'))
+
+    # Aggregate geoms by nuts region
+    level0 = Nuts.objects.values('n0cd', 'n0nm', 'n0nme', 'country').annotate(Union('geom'))
+
+    level1 = Nuts.objects.values('n0cd', 'n0nm', 'n0nme', 'n1cd', 'n1nm', 'country').annotate(Collect('geom'))
+    level2 = Nuts.objects.values('n0cd', 'n0nm', 'n0nme', 'n1cd', 'n1nm', 'n2cd', 'n2nm', 'country').annotate(Collect('geom'))
+
+    for reg in level0:
+        if not reg['geom__collect'].valid:
+            continue
+        print('-')
+        reg['geom'] = MultiPolygon(reg['geom__collect'])
+        del reg['geom__collect']
+        reg['level'] = 0
+        Nuts.objects.create(**reg)
+
+    #for reg in level1:
+        #reg['geom'] = MultiPolygon(reg['geom__collect'].envelope)
+        #del reg['geom__collect']
+        #reg['level'] = 1
+        #Nuts.objects.create(**reg)
+
+    #for reg in level2:
+        #reg['geom'] = MultiPolygon(reg['geom__collect'].envelope)
+        #del reg['geom__collect']
+        #reg['level'] = 2
+        #Nuts.objects.create(**reg)
