bdb50c69a1fec951b23e1ee6168e737c8a877c5d
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2016-04-13 09:51:37 +0100

    Changed url config to have a catch-all url for index. This is necessary for the url push state on Backbone.

--- protar/urls.py
+++ protar/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(router.urls)),
     url(r'^raster/', include('raster.urls')),
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^', TemplateView.as_view(template_name='index.html')),
 ]
