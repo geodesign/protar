a38df0dc5b39aee0a03fe95d26d3b0c9572da1d1
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2016-04-15 08:43:15 +0100

    Removed stale filter on nomenclatures.

--- frontend/js/protar/collections/nomenclatures.js
+++ frontend/js/protar/collections/nomenclatures.js
@@ -8,16 +8,7 @@ define([
 
     var Collection = Backbone.Collection.extend({
         model: Nomenclature,
-        url: '/api/nomenclature',
-        parse: function(data){
-            // Set color for unclassified to faint gray
-            _.each(data, function(dat){
-                if(dat.code_3 == '999' || dat.code_3 == '990'){
-                    dat.color = '#EEEEEE';
-                }
-            });
-            return data;
-        }
+        url: '/api/nomenclature'
     });
 
     return Collection;
